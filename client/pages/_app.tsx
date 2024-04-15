c4ef199a3ce6a630a54f363ed2025548bf486b88
Smart Contrart <smartcontrart@protonmail.com>
2024-04-15 10:45:10 -0400

    Added back the alchemy provider

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -35,7 +35,10 @@ const { chains, publicClient, webSocketPublicClient } = configureChains(
     sepolia,
     ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true" ? [sepolia] : []),
   ],
-  [publicProvider()]
+  [
+    alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_MAINNET }),
+    publicProvider(),
+  ]
 );
 
 const { wallets } = getDefaultWallets({
