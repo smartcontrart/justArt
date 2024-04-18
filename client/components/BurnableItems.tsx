098d9d34305eca31273b7e0c533db1c856493f0c
Smart Contrart <smartcontrart@protonmail.com>
2024-04-18 13:55:36 -0400

    Fixed image path

--- client/components/BurnableItems.tsx
+++ client/components/BurnableItems.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import Image from "next/image";
-import justArt from "../visuals/justArt_.svg";
+import justArt from "../visuals/justart_.svg";
 import { use } from "chai";
 import JustArt from "../contracts/JustArt.sol/JustArt.json";
 import {
